5243fbccf1e21d4cdfea230736f06fe59f962c2b
Leevar Williams <williams@renew.com>
2018-11-17 16:03:50 -0800

    Quote heading messages for readability

--- install-macos.sh
+++ install-macos.sh
@@ -23,52 +23,52 @@ function backup_and_symlink {
     ln -snf $src_path $dst_link
 }
 
-heading Create common directories
+heading "Create common directories"
 for d in .bin .profile.d .bash_profile.d .config; do
     info Creating $HOME/$d
     mkdir -p $HOME/$d
 done
 
-heading Install Bash config
+heading "Install Bash config"
 backup_and_symlink $BASE_DIR/bash/profile $HOME/.profile
 backup_and_symlink $BASE_DIR/bash/bash_profile $HOME/.bash_profile
 backup_and_symlink $BASE_DIR/bash/virtualenvify.sh $HOME/.bin/virtualenvify
 
-heading Install ssh config
+heading "Install ssh config"
 backup_and_symlink $BASE_DIR/ssh/config $HOME/.ssh/config
 backup_and_symlink $BASE_DIR/ssh/bash_profile_macos $HOME/.bash_profile.d/00-ssh
 
-heading Install Git config
+heading "Install Git config"
 backup_and_symlink $BASE_DIR/git/gitconfig $HOME/.gitconfig
 for f in $BASE_DIR/git/bin/*; do
     backup_and_symlink $f /usr/local/bin/$(basename $f)
 done
 
-heading Install Vim/Neovim config
+heading "Install Vim/Neovim config"
 backup_and_symlink $BASE_DIR/vim/init.vim $HOME/.vimrc
 backup_and_symlink $BASE_DIR/vim $HOME/.vim
 backup_and_symlink $BASE_DIR/vim $HOME/.config/nvim
 
-heading Install AWS config
+heading "Install AWS config"
 backup_and_symlink $BASE_DIR/aws $HOME/.aws
 
-heading Install RVM config
+heading "Install RVM config"
 backup_and_symlink $BASE_DIR/rvm/profile $HOME/.profile.d/10-rvm
 
-heading Install PyEnv config
+heading "Install PyEnv config"
 backup_and_symlink $BASE_DIR/pyenv/profile $HOME/.profile.d/20-pyenv
 
-heading Install Slate config
+heading "Install Slate config"
 backup_and_symlink $BASE_DIR/slate/slate.js $HOME/.slate.js
 
-heading Install Tmux config
+heading "Install Tmux config"
 backup_and_symlink $BASE_DIR/tmux/tmux.conf $HOME/.tmux.conf
 
-heading Install Yamllint config
+heading "Install Yamllint config"
 backup_and_symlink $BASE_DIR/yamllint $HOME/.config/yamllint
 
-heading Install Flake8 config
+heading "Install Flake8 config"
 backup_and_symlink $BASE_DIR/flake8/config $HOME/.config/flake8
 
-heading Install htop config
+heading "Install htop config"
 backup_and_symlink $BASE_DIR/htop $HOME/.config/htop
